37d014724cb3d5e99e9907517d137a18dc153f13
Nate Koenig <nkoenig@willowgarage.com>
2011-08-17 08:42:19 -0700

    Fixed the build

--- src/Server.cc
+++ src/Server.cc
@@ -1,162 +1,113 @@
-#include <boost/lexical_cast.hpp>
-#include <boost/algorithm/string.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
+#include <iostream>
+#include <valgrind/callgrind.h>
+#include <boost/interprocess/sync/interprocess_semaphore.hpp>
 
-#include "common/Timer.hh"
-#include "common/Exception.hh"
-
-#include "sdf/sdf.h"
-#include "sdf/sdf_parser.h"
-
-#include "sensors/Sensors.hh"
-
-#include "physics/Physics.hh"
-#include "physics/World.hh"
-#include "physics/Base.hh"
-
-#include "gazebo.h"
-#include "Master.hh"
+#include "gazebo_config.h"
+#include "common/CommonTypes.hh"
+#include "rendering/Rendering.hh"
+#include "common/SystemPaths.hh"
 #include "Server.hh"
 
-using namespace gazebo;
+gazebo::Server *server = NULL;
 
+std::string config_filename = "";
+gazebo::common::StrStr_M params;
 
-Server::Server()
+boost::interprocess::interprocess_semaphore sem(0);
+
+////////////////////////////////////////////////////////////////////////////////
+// TODO: Implement these options
+void PrintUsage()
 {
-  this->stop = false;
-
-  std::string host = "";
-  unsigned short port = 0;
-
-  gazebo::transport::get_master_uri(host,port);
-
-  this->master = new gazebo::Master();
-  this->master->Init(port);
-  this->master->Run();
+  std::cerr << "Usage: gzserver\n";
 }
 
-Server::~Server()
+////////////////////////////////////////////////////////////////////////////////
+// Print the version/licence string
+void PrintVersion()
 {
-  delete this->master;
+  fprintf(stderr, "%s", GAZEBO_VERSION_HEADER);
 }
 
-void Server::Load(const std::string &filename)
+////////////////////////////////////////////////////////////////////////////////
+// Parse the argument list.  Options are placed in static variables.
+int ParseArgs(int argc, char **argv)
 {
-  // Load gazebo
-  gazebo::load();
+  //FILE *tmpFile;
+  int ch;
 
-  // Load the world file
-  sdf::SDFPtr sdf(new sdf::SDF);
-  sdf::init(sdf);
-  sdf::readFile(filename, sdf);
-
-  /// Load the sensors library
-  sensors::load();
-
-  /// Load the physics library
-  physics::load();
-
-  sdf::ElementPtr worldElem = sdf->root->GetElement("world");
-  while(worldElem)
+  char *flags = (char*)("u");
+  // Get letter options
+  while ((ch = getopt(argc, argv, flags)) != -1)
   {
-    physics::WorldPtr world = physics::create_world();
-
-    //Create the world
-    try
+    switch (ch)
     {
-      physics::load_world(world, worldElem);
-    }
-    catch (common::Exception e)
-    {
-      gzthrow("Failed to load the World\n"  << e);
-    }
-
-    worldElem = sdf->root->GetNextElement("world", worldElem);
-  }
-}
-
-void Server::Init()
-{
-  sensors::init();
-
-  physics::init_worlds();
-}
-
-void Server::Stop()
-{
-  this->stop = true;
-}
-
-void Server::Fini()
-{
-  this->Stop();
-
-  gazebo::fini();
-
-  physics::fini();
-  sensors::fini();
-
-  this->master->Fini();
-  delete this->master;
-  this->master = NULL;
-}
-
-void Server::Run()
-{
-  // Run the gazebo, starts a new thread
-  gazebo::run();
-
-  // Run each world. Each world starts a new thread
-  physics::run_worlds();
-
-  // Update the sensors.
-  this->stop = false;
-  while (!this->stop)
-  {
-    sensors::run_once(true);
-    usleep(100000);
-  }
-
-  // Stop all the worlds
-  physics::stop_worlds();
-
-  sensors::stop();
-
-  // Stop gazebo
-  gazebo::stop();
-
-  // Stop the master 
-  this->master->Stop();
-}
-
-void Server::SetParams( const common::StrStr_M &params )
-{
-  common::StrStr_M::const_iterator iter;
-  for (iter = params.begin(); iter != params.end(); iter++)
-  {
-    if (iter->first == "pause")
-    {
-      bool p = false;
-      try
-      {
-        p = boost::lexical_cast<bool>(iter->second);
-      }
-      catch (...)
-      {
-        // Unable to convert via lexical_cast, so try "true/false" string
-        std::string str = iter->second;
-        boost::to_lower(str);
-
-        if (str == "true")
-          p = true;
-        else if (str == "false")
-          p = false;
-        else
-          gzerr << "Invalid param value[" << iter->first << ":" 
-                << iter->second << "]\n";
-      }
-
-      physics::pause_worlds(p);
+      case 'u':
+        params["pause"] = "true";
+        break;
+      default:
+        PrintUsage();
+        return -1;
     }
   }
+
+  argc -= optind;
+  argv += optind;
+
+  // Get the world file name
+  if (argc >= 1)
+    config_filename = argv[0];
+
+  return 0;
 }
 
+
+////////////////////////////////////////////////////////////////////////////////
+// sighandler to shut everything down properly
+void SignalHandler( int )
+{
+  server->Stop();
+}
+
+int main(int argc, char **argv)
+{
+  CALLGRIND_STOP_INSTRUMENTATION;
+
+  //Application Setup
+  if (ParseArgs(argc, argv) != 0)
+    return -1;
+
+  PrintVersion();
+
+  if (signal(SIGINT, SignalHandler) == SIG_ERR)
+  {
+    std::cerr << "signal(2) failed while setting up for SIGINT" << std::endl;
+    return -1;
+  }
+
+  server = new gazebo::Server();
+  server->Load(config_filename);
+  server->SetParams( params );
+  server->Init();
+  CALLGRIND_DUMP_STATS;
+  CALLGRIND_ZERO_STATS;
+  CALLGRIND_START_INSTRUMENTATION;
+
+  printf("RUNNING!\n");
+  server->Run();
+
+  CALLGRIND_DUMP_STATS;
+  CALLGRIND_ZERO_STATS;
+  CALLGRIND_STOP_INSTRUMENTATION;
+
+
+  server->Fini();
+
+  delete server;
+  server = NULL;
+  return 0;
+}
